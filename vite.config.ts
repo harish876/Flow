315352fac1938b768d61f981409e009db6992a45
Apratim Shukla <apratimshukla6@gmail.com>
2025-03-11 16:21:14 -0700

    Removed github actions

--- vite.config.ts
+++ vite.config.ts
@@ -11,10 +11,6 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  define: {
-    "import.meta.env.VITE_BACKEND_URL": JSON.stringify(process.env.VITE_BACKEND_URL),
-    "import.meta.env.VITE_FRONTEND_URL": JSON.stringify(process.env.VITE_FRONTEND_URL),
-  },
   build: {
     outDir: "dist",
     assetsDir: "assets",
